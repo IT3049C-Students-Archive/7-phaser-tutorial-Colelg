940175d116dddafbc65b2526963c3479854e718c
Colelg <55073850+Colelg@users.noreply.github.com>
2020-07-13 14:48:20 -0400

    Whoops, turns out my Git disconnected at some point or another so none of my Commits worked :/ Had to reset password but got it back

--- game.js
+++ game.js
@@ -1,6 +1,24 @@
-const { Phaser } = require("./phaser.min")
-
-window.onload = function() {
-  var game = new Phaser.game;
+var gameSettings = {
+  playerSpeed: 200,
+  maxPowerups: 2,
+  powerUpVel: 50,
 }
 
+var config = {
+  width: 256,
+  height: 272,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2],
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade:{
+        debug: false,
+        debugShowVelocity: false
+    }
+  },
+  autoCenter: true
+}
+
+
+var game = new Phaser.Game(config);
